19039af760555893b48b5c062cf351c9df60f16f
ThomasNam <purred@naver.com>
2020-05-05 22:28:46 +0900

    회원 가입 페이지 까지 생성

--- app.py
+++ app.py
@@ -1,30 +1,39 @@
 import os
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-dbfile = os.path.join(basedir, 'db.sqlite')
+from flask import render_template
+from model import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
 
 
-class Test(db.Model):
-    __tablename__ = 'test_table'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(32), unique=True)
+# class Test(db.Model):
+#     __tablename__ = 'test_table'
+#
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(32), unique=True)
+#
+# db.create_all()
 
-db.create_all()
+@app.route('/register')
+def regiter():
+    return render_template("register.html")
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template("hello.html")
 
 
 if __name__ == '__main__':
-    app.run()
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    dbfile = os.path.join(basedir, 'db.sqlite')
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbfile
+    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    db.app = app
+    db.create_all()
+
+    app.run(Debug=True)
